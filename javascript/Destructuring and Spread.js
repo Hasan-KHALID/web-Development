16b12ad9d8455275f46086ca15963724c4f7aff1
Hasan-KHALID <khalidhasan842@gmail.com>
2022-09-19 21:05:45 +0600

    dectructuring and spread

---
+++ javascript/Destructuring and Spread.js
@@ -0,0 +1,132 @@
+const fullStack = [
+  ['HTML', 'CSS', 'JS', 'React'],
+  ['Node', 'Express', 'MongoDB']
+]
+
+for(const [first, second, third] of fullStack) {
+console.log(first, second, third)
+}
+
+const countries = [['Finland', 'Helsinki'], ['Sweden', 'Stockholm'], ['Norway', 'Oslo']]
+
+for (const [country, city] of countries) {
+console.log(country, city)
+}
+
+// Destructuring Object
+
+const rectangle = {
+  width: 20,
+  height: 10,
+  area: 200
+}
+let { width, height, area, perimeter } = rectangle
+
+console.log(width, height, area, perimeter)
+
+const rectangle2 ={
+    width: 49,
+    height: 20,
+    area: 4000
+}
+
+let { width2, height2, area2, perimeter2} = rectangle2
+
+
+
+const rectangle3 = {
+  width: 20,
+  height: 10,
+  area: 200
+}
+let { width: w, height: h, area: a, perimeter: p } = rectangle
+
+console.log(w, h, a, p)
+
+
+const rectangle4 ={
+    width: 30,
+    height: 20,
+    area: 600
+}
+
+let {
+    width: e,
+    height: b,
+    area: c
+} = rectangle4
+
+console.log(e, b, c)
+
+
+
+// Without destructuring
+const rect = {
+  width: 20,
+  height: 10
+}
+const calculatePerimeter = rectangle => {
+  return 2 * (rectangle.width + rectangle.height)
+}
+
+console.log(calculatePerimeter(rect)) // 60
+//with destructuring
+
+
+
+const react ={
+    width: 30,
+    height: 80
+}
+
+const calculatePerimeter2 = rectangle2 => {
+    return 2 * (rectangle2.width + rectangle2.height)
+}
+console.log(calculatePerimeter(react))
+
+
+
+
+
+
+
+
+
+//Another Example
+const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  job: 'Instructor and Developer',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  languages: ['Amharic', 'English', 'Suomi(Finnish)']
+}
+// Let us create a function which give information about the person object without destructuring
+
+const getPersonInfo = obj => {
+  const skills = obj.skills
+  const formattedSkills = skills.slice(0, -1).join(', ')
+  const languages = obj.languages
+  const formattedLanguages = languages.slice(0, -1).join(', ')
+
+  personInfo = `${obj.firstName} ${obj.lastName} lives in ${obj.country}. He is  ${
+    obj.age
+  } years old. He is an ${obj.job}. He teaches ${formattedSkills} and ${
+    skills[skills.length - 1]
+  }. He speaks ${formattedLanguages} and a little bit of ${languages[2]}.`
+
+  return personInfo
+}
+
+console.log(getPersonInfo(person))
