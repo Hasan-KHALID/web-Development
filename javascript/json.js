0cc96ed1bc7b9f835c0f66781ed4b40b5ab836cb
Hasan-KHALID <khalidhasan842@gmail.com>
2023-01-08 23:59:58 +0600

    dd

--- javascript/json.js
+++ javascript/json.js
@@ -11,3 +11,15 @@ console.log(myJSON)
 
 const obj2 = {name: "John", age: 30, city: "New York"};
 const myJSON2 = JSON.stringify(obj);
+
+
+
+
+const myObj = { name: "John", age: 31, city: "New York" };
+const myJSON1 = JSON.stringify(myObj);
+localStorage.setItem("testJSON", myJSON);
+
+// Retrieving data:
+let text4 = localStorage.getItem("testJSON");
+let obj5 = JSON.parse(text);
+document.getElementById("demo").innerHTML = obj.name;
