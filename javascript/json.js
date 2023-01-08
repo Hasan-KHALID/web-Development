65a6b71c6c551b8be6f338b4ab4d2e53db3e0d66
Hasan-KHALID <khalidhasan842@gmail.com>
2023-01-08 23:29:00 +0600

    modefied

---
+++ javascript/json.js
@@ -0,0 +1,13 @@
+const obj = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
+
+const text = '["Ford", "BMW", "Audi", "Fiat"]';
+const myArr = JSON.parse(text);
+
+
+
+const obj1 = {name: "John", age: 30, city: "New York"};
+const myJSON = JSON.stringify(obj);
+console.log(myJSON)
+
+const obj2 = {name: "John", age: 30, city: "New York"};
+const myJSON2 = JSON.stringify(obj);
