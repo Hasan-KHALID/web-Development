4949b13ff5b24a73215da6a7229c09fd805d15db
Hasan-KHALID <khalidhasan842@gmail.com>
2023-01-09 22:53:34 +0600

    web-api

--- javascript/json.js
+++ javascript/json.js
@@ -33,3 +33,24 @@ xmlhttp.onload = function() {
 };
 xmlhttp.open("GET", "json_demo.txt");
 xmlhttp.send();
+
+const myObj44 = {name: "John", age: 31, city: "New York"};
+const myJSON09 = JSON.stringify(myObj);
+window.location = "demo_json.php?x=" + myJSON;
+
+
+
+const dbParam = JSON.stringify({table:"customers",limit:20});
+const xmlhttp = new XMLHttpRequest();
+xmlhttp.onload = function() {
+  myObj = JSON.parse(this.responseText);
+  let text = "<table border='1'>"
+  for (let x in myObj) {
+    text += "<tr><td>" + myObj[x].name + "</td></tr>";
+  }
+  text += "</table>"
+  document.getElementById("demo").innerHTML = text;
+}
+xmlhttp.open("POST", "json_demo_html_table.php");
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam);
