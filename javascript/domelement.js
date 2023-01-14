e42a64e5829c8e1a87fd805cf0b548ccdd8ddc22
Hasan-KHALID <khalidhasan842@gmail.com>
2023-01-14 14:20:24 +0600

    queue

--- javascript/domelement.js
+++ javascript/domelement.js
@@ -1,1 +1,5 @@
-console.log(document.title)
+const heder1 = document.getElementById('head')
+const grandparent = document.querySelector('.container')
+const parents = grandparent.children
+
+console.log(typeof(parents[1]))
---
+++ javascript/queue2.js
@@ -0,0 +1,70 @@
+
+// Queue class
+class Queue
+{
+	// Array is used to implement a Queue
+	constructor()
+	{
+		this.items = [];
+	}
+	isEmpty()
+	{
+		// return true if the queue is empty.
+		return this.items.length == 0;
+	}
+	enqueue(element)
+	{
+		// adding element to the queue
+		this.items.push(element);
+		document.write(element + " enqueued to queue<br>");
+	}
+	dequeue()
+	{
+		// removing element from the queue
+		// returns underflow when called
+		// on empty queue
+		if(this.isEmpty())
+			return "Underflow<br>";
+	    return this.items.shift();
+	}
+	front()
+	{
+		// returns the Front element of
+		// the queue without removing it.
+		if(this.isEmpty())
+			return "No elements in Queue<br>";
+		return this.items[0];
+	}
+	rear()
+	{
+		// returns the Rear element of
+		// the queue without removing it.
+		if(this.isEmpty())
+			return "No elements in Queue<br>";
+		return this.items[this.items.length-1];
+	}
+}
+
+// creating object for queue class
+var queue = new Queue();
+
+// Adding elements to the queue
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+
+// queue contains [10, 20, 30, 40]
+// removes 10
+document.write(queue.dequeue() + " dequeued from queue<br>");
+
+// queue contains [20, 30, 40]
+// Front is now 20
+document.write("Front item is " + queue.front() + "<br>");
+
+// printing the rear element
+// Rear is 40
+document.write("Rear item is " + queue.rear() + "<br>");
+
+// This code is contributed by Susobhan Akhuli
+
