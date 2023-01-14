9a22c6e773e6748720d73ee796df4d17cb724319
Hasan-KHALID <khalidhasan842@gmail.com>
2023-01-14 23:37:00 +0600

    updated

--- javascript/array-manipulation.js
+++ javascript/array-manipulation.js
@@ -24,3 +24,24 @@ var a = [ 1, 3, 2, 5, 8 ];
 var n = a.length;
 document.write(getElement(a, n, S));
 
+
+
+function arrayManipulation(n, queries){
+    let arr = Array(n+2).fill(0)
+
+    for( let i =0 ; i < queries.length; i++ ){
+        let [a,b,k] = queries[i]
+
+        arr[a] += k
+        arr[b+1] -=k 
+    }
+    let max = Number.MIN_VALUE
+    let sum = 0
+
+    for(let i = 0; i<arr.length; i++){
+        sum +=arr[i]
+        max = Math.max(max.sum)
+    }
+    return max
+}
+
